fc6eee2e866145433c2b5fd05d31bab99946d67a
GozdeEBaran <eski.gozdee@gmail.com>
2026-02-22 20:22:16 -0500

    fix: correct import path for MovieModel and update movie resolvers structure

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,3 +1,23 @@
-import MovieModel from '../models/movie.js';
+import MovieModel from '../models/Movie.js';
 
 // Resolvers define the technique for fetching the types defined in the schema.
+const movieResolvers = {
+  Query: {
+    // a. Get all movies
+    getAllMovies: async () => {
+      return await MovieModel.find();
+    },
+
+    // b. Get movie by ID
+    getMovieById: async (_, { id }) => {
+      return await MovieModel.findById(id);
+    },
+
+    // c. Get movies by Director name using static methods
+    getMoviesByDirector: async (_, { director_name }) => {
+      return await MovieModel.findByDirector(director_name);
+    },
+  },
+};
+
+export default movieResolvers;
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,1 +1,20 @@
 import { gql } from 'graphql-tag';
+
+const movieSchema = gql`
+  type Movie {
+    id: ID!
+    name: String!
+    director_name: String!
+    production_house: String!
+    release_date: String!
+    rating: Float!
+  }
+
+  type Query {
+    getAllMovies: [Movie]
+    getMovieById(id: ID!): Movie
+    getMoviesByDirector(director_name: String!): [Movie]
+  }
+`;
+
+export default movieSchema;
